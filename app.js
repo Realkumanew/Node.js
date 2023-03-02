b1e4f7a1c9c653075891cb4373f91dbad749305f
Realkumanew <wiparat.md@gmail.com>
2023-03-02 21:17:34 +0700

    Add html

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, "/public/")))
 app.get("/", (req, res) => {
     res.send("Hello World Ja");
 })
